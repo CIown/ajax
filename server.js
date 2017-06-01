5352efd5b75e312a2d918982a9255454b44a3b4e
qidi <1072359114@qq.com>
2017-06-01 22:49:59 +0800

    ajax

--- server.js
+++ server.js
@@ -12,18 +12,21 @@ var server = http.createServer(function(request, response){
   var query = temp.query
   var method = request.method
 
-  //从这里开始看，上面不要看
-  if(path){
-    var string = fs.readFileSync("./index.html")
+
+  if(path ==='/'){
+    var string = fs.readFileSync('./index.html')
     response.setHeader('Content-Type','text/html; charset="utf-8"')
     response.end(string)
   }else if(path ==='/page.html'){
     var string = fs.readFileSync("./page.html")
     response.setHeader('Content-Type','text/html; charset ="utf-8"')
-    response.end("i am wrong")
+    response.end(string)
+  }else if(path ==='/xxx'){
+     response.setHeader('Content-Type','text/html; charset ="utf-8"')
+    response.end('')
   }
 
-  // 代码结束，下面不要看
+  
   console.log(method + ' ' + request.url)
 })
 
